e4195479256ceebf1b7307b139249f59376e4cc7
Gustavo Dias <gustavo7dias@gmail.com>
2025-11-02 16:06:00 -0300

    Update Footer responsivity

--- app/components/Footer.vue
+++ app/components/Footer.vue
@@ -1,40 +1,55 @@
 <template>
-  <footer class="pt-12 pb-4 bg-cyan-500">
+  <footer class="pt-6 sm:pt-12 pb-4 bg-cyan-500">
     <div class="container text-white">
-      <div class="flex justify-center mb-12 gap-28">
+      <div
+        class="flex flex-col sm:flex-row items-center sm:items-start sm:justify-center mb-6 md:mb-12 gap-4 sm:gap-6 md:gap-20 lg:gap-28">
         <div>
-          <img src="~assets/img/dma-logo-white.svg" alt="DMA">
+          <img class="w-[200px] sm:w-32 md:w-[200px]" src="~assets/img/dma-logo-white.svg" alt="DMA">
         </div>
-        <div class="flex gap-8">
+        <div class="flex gap-2 md:gap-4 lg:gap-8">
           <ul>
-            <li><a class="flex items-center px-8 h-12 bg-cyan-600 mb-2 border-l-[3px] border-white"
+            <li><a
+                class="text-xs md:text-sm lg:text-base flex items-center px-2 md:px-4 lg:px-8 h-12 bg-cyan-600 mb-2 border-l-[3px] border-white"
                 href="/">Comunità</a></li>
-            <li><a class="flex items-center px-8 h-12 bg-cyan-600 mb-2 border-l-[3px] border-white" href="/">Media</a>
+            <li><a
+                class="text-xs md:text-sm lg:text-base flex items-center px-2 md:px-4 lg:px-8 h-12 bg-cyan-600 mb-2 border-l-[3px] border-white"
+                href="/">Media</a>
             </li>
-            <li><a class="flex items-center px-8 h-12 bg-cyan-600 mb-2 border-l-[3px] border-white" href="/">DMA nel
+            <li><a
+                class="text-xs md:text-sm lg:text-base flex items-center px-2 md:px-4 lg:px-8 h-12 bg-cyan-600 mb-2 border-l-[3px] border-white"
+                href="/">DMA nel
                 Mondo</a></li>
-            <li><a class="flex items-center px-8 h-12 bg-cyan-600 mb-2 border-l-[3px] border-white" href="/">Chi
+            <li><a
+                class="text-xs md:text-sm lg:text-base flex items-center px-2 md:px-4 lg:px-8 h-12 bg-cyan-600 border-l-[3px] border-white"
+                href="/">Chi
                 Siamo</a></li>
           </ul>
           <ul>
-            <li><a class="flex items-center px-8 h-12 bg-cyan-600 mb-2 border-l-[3px] border-white"
+            <li><a
+                class="text-xs md:text-sm lg:text-base flex items-center px-2 md:px-4 lg:px-8 h-12 bg-cyan-600 mb-2 border-l-[3px] border-white"
                 href="/">Soluzioni</a></li>
-            <li><a class="flex items-center px-8 h-12 bg-cyan-600 mb-2 border-l-[3px] border-white" href="/">Premi</a>
+            <li><a
+                class="text-xs md:text-sm lg:text-base flex items-center px-2 md:px-4 lg:px-8 h-12 bg-cyan-600 mb-2 border-l-[3px] border-white"
+                href="/">Premi</a>
             </li>
-            <li><a class="flex items-center px-8 h-12 bg-cyan-600 mb-2 border-l-[3px] border-white"
+            <li><a
+                class="text-xs md:text-sm lg:text-base flex items-center px-2 md:px-4 lg:px-8 h-12 bg-cyan-600 mb-2 border-l-[3px] border-white"
                 href="/">Contattaci</a></li>
-            <li><a class="flex items-center px-8 h-12 bg-cyan-600 mb-2 border-l-[3px] border-white" href="/">Privacy e
+            <li><a
+                class="text-xs md:text-sm lg:text-base flex items-center px-2 md:px-4 lg:px-8 h-12 bg-cyan-600 border-l-[3px] border-white"
+                href="/">Privacy e
                 Protezione</a>
             </li>
           </ul>
         </div>
         <div class="flex flex-row sm:flex-col gap-2">
           <a href="/"><img class="w-10 h-10" src="~assets/img/linkedin-icon-white.svg" alt="LinkedIn"></a>
+          <!-- 
           <a href="/"><img class="w-10 h-10" src="~assets/img/instagram-icon-white.svg" alt="Instagram"></a>
-          <a href="/"><img class="w-10 h-10" src="~assets/img/facebook-icon-white.svg" alt="Facebook"></a>
+          <a href="/"><img class="w-10 h-10" src="~assets/img/facebook-icon-white.svg" alt="Facebook"></a> -->
         </div>
       </div>
-      <p class="text-center">Tutti i diritti riservati – DMA Technology and Digital S.A.</p>
+      <p class="text-sm sm:text-base text-center">Tutti i diritti riservati – DMA Technology and Digital S.A.</p>
     </div>
   </footer>
 </template>
