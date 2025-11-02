cefff3867c6e1c506ee4827e59412b3992f8e642
Gustavo Dias <gustavo7dias@gmail.com>
2025-11-02 13:46:52 -0300

    Update Award responsivity

--- app/components/Award.vue
+++ app/components/Award.vue
@@ -1,23 +1,30 @@
 <template>
-  <section class="bg-neutral-500 py-10">
+  <section class="bg-neutral-500 pt-6 pb-0 sm:py-10">
     <div class="container text-center text-white">
       <h2 class="text-2xl font-bold mb-2 leading-10">PREMI</h2>
       <p class="text-base max-w-[492px] mx-auto">
         Molti casi dei nostri clienti ottengono riconoscimenti nazionali e
         internazionali con DMA.
       </p>
-      <div class="py-10">
-        <div class="flex items-center justify-center gap-10 mb-8">
-          <img class="h-[184px]" src="~assets/img/image80.png" alt="Image 1">
-          <img class="h-[172px]" src="~assets/img/image88.png" alt="Image 2">
-          <img class="h-[198px]" src="~assets/img/image85.png" alt="Image 3">
-          <img class="h-[179px]" src="~assets/img/Group30.png" alt="Image 4">
+      <div class="py-8 md:py-10">
+        <div class="flex items-center justify-center gap-8 sm:gap-10 mb-4 sm:mb-8 flex-col sm:flex-row">
+          <div class="flex gap-8 sm:gap-10">
+            <img class="h-[119px] sm:h-[76px] md:h-[138px] lg:h-[184px]" src="~assets/img/image80.png" alt="Image 1">
+            <img class="h-[111px] sm:h-[71px] md:h-[129px] lg:h-[172px]" src="~assets/img/image88.png" alt="Image 2">
+          </div>
+          <div class="flex gap-8 sm:gap-10">
+            <img class="h-[128px] sm:h-[82px] md:h-[149px] lg:h-[198px]" src="~assets/img/image85.png" alt="Image 3">
+            <img class="h-[115px] sm:h-[74px] md:h-[134px] lg:h-[179px]" src="~assets/img/Group30.png" alt="Image 4">
+          </div>
         </div>
-        <div class="flex items-center justify-center gap-10">
-          <img class="h-[122px]" src="~assets/img/image82.png" alt="Image 5">
-          <img class="h-[256px]" src="~assets/img/image87.png" alt="Image 6">
-          <img class="h-[145px]" src="~assets/img/image83.png" alt="Image 7">
-          <img class="h-[94px]" src="~assets/img/image89.png" alt="Image 8">
+
+        <div class="flex items-center justify-center gap-8 sm:gap-10 flex-col sm:flex-row">
+          <img class="h-[78px] sm:h-[50px] md:h-[91px] lg:h-[122px]" src="~assets/img/image82.png" alt="Image 5">
+          <div class="flex items-center gap-8 sm:gap-10">
+            <img class="h-[165px] sm:h-[106px] md:h-[192px] lg:h-[256px]" src="~assets/img/image87.png" alt="Image 6">
+            <img class="h-[93px] sm:h-[60px] md:h-[109px] lg:h-[145px]" src="~assets/img/image83.png" alt="Image 7">
+          </div>
+          <img class="h-[60px] sm:h-[38px] md:h-[70px] lg:h-[94px]" src="~assets/img/image89.png" alt="Image 8">
         </div>
       </div>
     </div>
