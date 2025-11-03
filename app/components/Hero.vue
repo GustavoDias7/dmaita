b8fb0987d46a265e3845cc1bd42019259d677b5e
Gustavo Dias <gustavo7dias@gmail.com>
2025-11-02 22:28:03 -0300

    Update title and subtitle from Hero

--- app/components/Hero.vue
+++ app/components/Hero.vue
@@ -1,16 +1,17 @@
 <template>
   <main>
-    <div class="container mb-12 pt-14 sm:pt-8 lg:pt-12">
+    <div class="container mb-6 md:mb-12 pt-14 sm:pt-8 lg:pt-12">
       <video autoplay muted loop class="mx-auto hidden sm:block sm:h-[185px] md:h-[294px] lg:h-[378px]">
         <source src="~assets/video/main-hero-image-dma.mp4" type="video/mp4">
       </video>
       <div class="banner"></div>
-      <div class="flex max-w-[800px] gap-8 mx-auto">
-        <h1 class="text-xl font-bold text-right">
-          TRASFORMA L'ESPERIENZA DEL SERVIZIO E LA SICUREZZA DEI <br> TUOI CLIENTI.
+      <div class="flex flex-col md:flex-row max-w-[800px] gap-2 md:gap-8 mx-auto">
+        <h1 class="text-xl font-bold md:text-right">
+          TRASFORMA L'ESPERIENZA DEL SERVIZIO E LA SICUREZZA DEI <br class="hidden md:block"> TUOI CLIENTI.
         </h1>
         <p class="text-base">
-          DMA è la trasformazione digitale che accelera le tue prestazioni, aumenta il tuo fatturato e il tuo <br> NPS.
+          DMA è la trasformazione digitale che accelera le tue prestazioni, aumenta il tuo fatturato e il tuo <br
+            class="hidden md:block"> NPS.
           E riduce anche i tuoi costi.
         </p>
       </div>
@@ -19,8 +20,13 @@
 </template>
 
 <style>
+.banner {
+  display: none;
+}
+
 @media (max-width: 480px) {
   .banner {
+    display: block;
     height: 168px;
     background-image: url("assets/img/banner-mobile.webp");
     background-size: cover;
