e3daefb529759531ec515698ae40c9d8fb4aa093
Gustavo Dias <gustavo7dias@gmail.com>
2025-11-03 14:17:31 -0300

    Add SEO meta tags

--- app/app.vue
+++ app/app.vue
@@ -16,3 +16,39 @@
     <Footer />
   </div>
 </template>
+<script setup>
+import logo from "assets/img/dma-logo.png";
+
+useSeoMeta({
+  // 🔹 Basic SEO
+  title: 'DMA - Digital Made Accessible',
+  description: 'DMA è la trasformazione digitale che accelera le tue prestazioni, aumenta il tuo fatturato e il tuo NPS. E riduce anche i tuoi costi.',
+  applicationName: 'DMA - Digital Made Accessible',
+  keywords: 'marketing digitale, email marketing, comunicazione aziendale, gestione campagne email, infrastruttura di hosting, sistema di invio email, automazione del marketing, interfaccia utente semplice, sicurezza dei dati, costi ridotti, piattaforma di marketing, assistenza clienti efficace, monitoraggio delle email, report delle prestazioni, soluzione di messaggistica professionale',
+  author: 'DMA - Digital Made Accessible',
+  robots: 'index, follow',
+  canonical: 'https://dmaita.com/',
+
+  // 🔹 Open Graph (Facebook, LinkedIn, etc.)
+  ogTitle: 'DMA - Digital Made Accessible',
+  ogDescription: 'DMA è la trasformazione digitale che accelera le tue prestazioni, aumenta il tuo fatturato e il tuo NPS. E riduce anche i tuoi costi.',
+  ogImage: logo,
+  ogType: 'website',
+  ogUrl: 'https://dmaita.com/',
+  ogSiteName: 'DMA - Digital Made Accessible',
+  ogLocale: 'it_IT',
+
+  // 🔹 Twitter Card
+  twitterCard: 'summary_large_image',
+  twitterTitle: 'DMA - Digital Made Accessible',
+  twitterDescription: 'DMA è la trasformazione digitale che accelera le tue prestazioni, aumenta il tuo fatturato e il tuo NPS. E riduce anche i tuoi costi.',
+  twitterImage: logo,
+  // twitterSite: '@your_twitter_handle',
+  // twitterCreator: '@your_twitter_handle',
+
+  // 🔹 Additional meta
+  themeColor: '#2EA7AF',
+  // colorScheme: 'light dark',
+  viewport: 'width=device-width, initial-scale=1.0',
+})
+</script>
